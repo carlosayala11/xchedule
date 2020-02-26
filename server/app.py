f7b6f445789921d6c10891e91db6f3b71aba105a
Carlos Ayala <carlos.ayala11@upr.edu>
2020-02-25 23:16:06 -0400

    Connected Flask app with DB

--- server/app.py
+++ server/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from handler.userHandler import UserHandler
+import psycopg2
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/df6hbif2dks1kv'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/df6hbif2dks1kv'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
 
 
 
--- server/dao/user.py
+++ server/dao/user.py
@@ -1,13 +1,15 @@
-from config.config import pg_config
+# from config.config import pg_config
 import psycopg2
 
 
 class UsersDAO:
     def __init__(self):
-        connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
-                                                            pg_config['user'],
-                                                            pg_config['passwd'])
-        self.conn = psycopg2._connect(connection_url)
+        # connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
+        #                                                     pg_config['user'],
+        #                                                     pg_config['passwd'])
+        # self.conn = psycopg2._connect(connection_url)
+        DATABASE_URL = 'postgres://ridrboqkilxrvh:d973fc864df2f973135c7280756679636ae84f8964f56e5882aa1291b0719c24@ec2-52-73-247-67.compute-1.amazonaws.com:5432/df6hbif2dks1kv'
+        self.conn = psycopg2.connect(DATABASE_URL)
 
     def getAllUsers(self):
         cursor = self.conn.cursor()
