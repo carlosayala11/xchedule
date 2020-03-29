4c22ddab4dc3a4f27d3991cc8cc1f01c8cff8945
rexkater <rex.reyes@upr.edu>
2020-03-28 21:11:24 -0400

    Profile page created.

---
+++ client/src/pages/Home.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import '../styles/Home.css'
+
+class Home extends Component{
+    constructor(){
+        super();
+        this.state={
+        }
+
+    }
+
+    render(){
+        return(
+            <div>
+                <p>Profile</p>
+            </div>
+
+
+        )
+    }
+}
+
+export default Home;
