5854e21db8f6ef3d5e8e7737064665649f4ab29c
yetsiel <yetsiel.aviles@upr.edu>
2020-03-30 17:50:06 -0400

    Added business handler and business DAO

---
+++ server/dao/business.py
@@ -0,0 +1,67 @@
+from config.config import pg_config
+import psycopg2
+
+class BusinessDAO:
+    def __init__(self):
+        # connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
+        #                                                     pg_config['user'],
+        #                                                     pg_config['passwd'])
+        # self.conn = psycopg2._connect(connection_url)
+        DATABASE_URL = 'postgres://ridrboqkilxrvh:d973fc864df2f973135c7280756679636ae84f8964f56e5882aa1291b0719c24@ec2-52-73-247-67.compute-1.amazonaws.com:5432/df6hbif2dks1kv'
+        self.conn = psycopg2.connect(DATABASE_URL)
+
+    def getAllBusiness(self):
+        cursor = self.conn.cursor()
+        query = "select * from business;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getBusinessById(self, bid):
+        cursor = self.conn.cursor()
+        query = "select * from business where bid = %s;"
+        cursor.execute(query, (bid,))
+        result = cursor.fetchone()
+        return result
+
+    def getServicesByBusinessId(self, bid):
+        cursor = self.conn.cursor()
+        result = []
+        query = "select sid, details, website_url, facebook, instagram from business natural inner join service natural inner join offers where bid =%s;"
+        cursor.execute(query, (bid,))
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getBusinessByCity(self, city):
+        cursor = self.conn.cursor()
+        query = "select * from business where baddress.city = %s;"
+        cursor.execute(query, (city,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def insert(self, bname, bphone, bemail, baddress, blocation):
+        cursor = self.conn.cursor()
+        query = "insert into business(bname, bphone, bemail, baddress, blocation) values (%s, %s, %s, %s, %s) returning bid;"
+        cursor.execute(query, (bname, bphone, bemail, baddress, blocation))
+        bid = cursor.fetchone()[0]
+        self.conn.commit()
+        return bid
+
+    def delete(self, bid):
+        cursor = self.conn.cursor()
+        query = "delete from business where bid = %s;"
+        cursor.execute(query, (bid,))
+        self.conn.commit()
+        return bid
+
+    def update(self, bid, bname, bphone, bemail, baddress, blocation):
+        cursor = self.conn.cursor()
+        query = "update person set bname = %s, bphone = %s, bemail = %s, baddress = %s, blocation = %s where bid = %s;"
+        cursor.execute(query, (bname, bphone, bemail, baddress, blocation, bid,))
+        self.conn.commit()
+        return bid
