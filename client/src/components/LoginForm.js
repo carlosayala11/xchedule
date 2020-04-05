788bbc99a407c6c6f35e5daf25473a48ca833001
Gabriel Rosario <gabriel.rosario2@upr.edu>
2020-04-05 18:20:39 -0400

    added Forgot Password Functionality

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
-import { Button, Input } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter,Input } from 'reactstrap';
 import * as firebase from 'firebase';
 import '../styles/Login.css'
 import {Redirect} from 'react-router-dom'
+import { Spinner } from 'reactstrap';
 
 
 class LoginForm extends Component{
@@ -11,9 +12,12 @@ class LoginForm extends Component{
         this.state={
             email:'',
             password:'',
-            loggedIn: false
+            loggedIn: false,
+            loadingLoggin: false,
+            modal:false,
+            emailreset:''
         }
-
+        this.renderLoginSpinner = this.renderLoginSpinner.bind(this)
     
     }
 
@@ -27,17 +31,15 @@ class LoginForm extends Component{
     }
 
     loginExistingUser(){
+        this.setState({loadingLoggin:true})
         firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then(()=>{
             console.log("SUCCESS")
             this.setState({
                 loggedIn: true 
-            })
-            
+            })            
+        }).catch((error) => {
+            this.setState({loadingLoggin:false})
 
-        }
-
-            
-        ).catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
@@ -47,19 +49,56 @@ class LoginForm extends Component{
           });
     }
 
+    renderLoginSpinner(){
+        if(this.state.loadingLoggin){
+            return <Spinner color="primary" />
+        }else{
+            return <p>Login</p>
+        }
+    }
 
+    toggle(){
+        this.setState(prevState => ({
+            modal: !prevState.modal
+        }))
+    }
+
+    sendPasswordReset(){
+        firebase.auth().sendPasswordResetEmail(this.state.emailreset).then(function() {
+            console.log("EMAIL SENT")
+            // Email sent.
+          }).catch(function(error) {
+            // An error happened.
+            console.log("Error")
+          });
+    }
+
+    
     render(){
         if (this.state.loggedIn) {
             return <Redirect to='/profile'/>;
-          }
+        }
         return(
             <div className="login-form">
+                <Modal isOpen={this.state.modal} toggle={this.toggle.bind(this)}>
+                    <ModalHeader toggle={this.toggle.bind(this)}>Send Password Reset Email</ModalHeader>
+                    <ModalBody>
+                        <p>Enter your E-Mail to send a password reset email:</p>
+                        <Input name="emailreset" onChange={this.onInputChange} placeholder="sample@email.com"></Input>
+                    </ModalBody>
+                    <ModalFooter>
+                    <Button color="primary" onClick={this.sendPasswordReset.bind(this)}>Do Something</Button>{' '}
+                    <Button color="secondary" onClick={this.toggle.bind(this)}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>                
                 <p className="login-title">Login</p>
                 
                 <Input className="login-input" name="email" placeholder="email@email.com" onChange={this.onInputChange}/>
                 <Input type="password" className="login-input" name="password" placeholder="password123" onChange={this.onInputChange}/>
-                <p className="login-small-text">Forgot Password?</p>
-                <Button className="login-button" color="primary" onClick={this.loginExistingUser.bind(this)}>Login</Button>{' '}                
+                <Button onClick={this.toggle.bind(this)} className="login-small-text">Forgot Password?</Button>
+                <Button className="login-button" color="primary" onClick={this.loginExistingUser.bind(this)}>
+                    {this.renderLoginSpinner()}
+                </Button>               
             </div>
             
 
